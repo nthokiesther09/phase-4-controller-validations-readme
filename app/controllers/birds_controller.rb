fe3ee2903efea14c09ef8ce62d39253286982490
nthokiesther09 <nthokiesther09@gmail.com>
2023-03-17 18:58:33 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   # GET /birds
   def index
     birds = Bird.all
@@ -9,8 +9,20 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
+  
+      bird = Bird.create!(bird_params)
+      render json: bird, status: :created
+    # rescue ActiveRecord::RecordInvalid => invalid
+    #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+
+  #   bird = Bird.create(bird_params)
+  # if bird.valid?
+  #   render json: bird, status: :created
+  # else
+  #   render json: { errors: bird.errors }, status: :unprocessable_entity
+  # end
+    # bird = Bird.create(bird_params)
+    # render json: bird, status: :created
   end
 
   # GET /birds/:id
@@ -20,10 +32,17 @@ class BirdsController < ApplicationController
   end
 
   # PATCH /birds/:id
+  # def update
+  #   bird = find_bird
+  #   bird.update(bird_params)
+  #   render json: bird
+  # end
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
@@ -45,6 +64,11 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+  
+  def render_unprocessable_entity_response(invalid)
+    # render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
